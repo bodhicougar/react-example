e11c2e6708f5c1bf1c76b2aaeac6bcbfcd60777d
Randall Knutson <randall@form.io>
2016-06-23 14:51:30 -0500

    Add example of onElementRender

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,12 @@ var ReactDOM = require('react-dom');
 var Formio = require('react-formio/src/Formio.jsx');
 
 var form = require('./complex.json');
+var submission = {
+  data: {
+    textField: 'My Value',
+    number: 300
+  }
+};
 
 var formChange = function(submission) {
   console.log('change', submission);
@@ -12,6 +18,24 @@ var formSubmit = function(submission) {
   console.log('submit', submission);
 }
 
+var onElementRender = function(component, element) {
+  if (submission.data.hasOwnProperty(component.props.component.key) && submission.data[component.props.component.key].toString() !== component.state.value.toString()) {
+    return (
+      <div>
+        {element}
+        <div style={{color: 'red'}}>Original Value: {submission.data[component.props.component.key]}</div>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        {element}
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <Formio src="https://examples.form.io/components" onChange={formChange} onFormSubmit={formSubmit}></Formio>, document.getElementById('formio')
+  <Formio src="https://examples.form.io/components" submission={submission} onChange={formChange} onFormSubmit={formSubmit} onElementRender={onElementRender}></Formio>, document.getElementById('formio')
 );
