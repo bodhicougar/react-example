9c1dff9b6683a4784fef283f97bd8efacdd1b818
Randall Knutson <randall@form.io>
2016-07-01 08:33:05 -0500

    Add css loader for required CSS.

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
         }
       },
       {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.png$|\.gif$|\.svg|\.ttf|\.woff|\.eot/,
+        loader: "file-loader"
+      },
+      {
         test: /\.json$/,
         loader: 'json'
       }
